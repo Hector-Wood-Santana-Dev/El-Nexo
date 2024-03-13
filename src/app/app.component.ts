166a76ec1f22fd70f15f56475df3d6a6a78b65ea
itzha <itzhakprieto@gmail.com>
2024-03-13 11:57:28 +0000

    Create Home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HeaderComponent} from "./Componentes/header/header.component";
+import {HomeComponent} from "./Paginas/home/home.component";
+import {LoginComponent} from "./Paginas/login/login.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, HomeComponent, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
