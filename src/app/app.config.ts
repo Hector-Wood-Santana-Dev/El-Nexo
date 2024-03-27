19aa18208482df37a491e991bf891c9403d703a0
itzha <itzhakprieto@gmail.com>
2024-03-27 10:56:33 +0000

    link to firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,10 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"el-nexo-ps","appId":"1:201490379053:web:84abadd25ae8e7d7f9e3a4","storageBucket":"el-nexo-ps.appspot.com","apiKey":"AIzaSyAAtjlz_iEiNM8-1VV7tWb8wS1uxy2Ms_c","authDomain":"el-nexo-ps.firebaseapp.com","messagingSenderId":"201490379053"}))), importProvidersFrom(provideFirestore(() => getFirestore()))]
 };
