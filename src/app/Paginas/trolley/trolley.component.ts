24713373b3f166d2f4d51c80ef2bb75b47623b73
itzha <itzhakprieto@gmail.com>
2024-04-19 14:09:26 +0100

    configuracion del boton de pagar

--- src/app/Paginas/trolley/trolley.component.ts
+++ src/app/Paginas/trolley/trolley.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {TrolleyServiceService} from "../../service/trolley-service.service";
 import {Product} from "../../interface/product";
-import {NgForOf} from "@angular/common";
+import {NgForOf, NgIf} from "@angular/common";
 import 'sweetalert2/src/sweetalert2.scss'
 import Swal from 'sweetalert2';
 import {Router} from "@angular/router";
@@ -9,7 +9,8 @@ import {Router} from "@angular/router";
   selector: 'app-trolley',
   standalone: true,
   imports: [
-    NgForOf
+    NgForOf,
+    NgIf
   ],
   templateUrl: './trolley.component.html',
   styleUrl: './trolley.component.css'
