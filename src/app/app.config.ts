9c102ca60ecf9a9e4d5dd3140467b93acd72ee4c
Héctor Wood Santana <95436828+Hector-Wood-Santana-Dev@users.noreply.github.com>
2024-04-19 14:06:17 +0100

    Update app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,5 +7,5 @@ import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 //TODO: Recordar añadir clave en local. NO subir clave al repositorio.
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"el-nexo-ps","appId":"1:201490379053:web:84abadd25ae8e7d7f9e3a4","storageBucket":"el-nexo-ps.appspot.com","apiKey":"AIzaSyAAtjlz_iEiNM8-1VV7tWb8wS1uxy2Ms_c","authDomain":"el-nexo-ps.firebaseapp.com","messagingSenderId":"201490379053"}))), importProvidersFrom(provideFirestore(() => getFirestore()))]
+  providers: 
 };
