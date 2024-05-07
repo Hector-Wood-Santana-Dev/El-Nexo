692291b9b932c6beb6a70fbe2536660b19b06566
itzha <itzhakprieto@gmail.com>
2024-05-07 14:06:54 +0100

    admin pages finish

--- src/app/Componentes/formulario-add/formulario-add.component.ts
+++ src/app/Componentes/formulario-add/formulario-add.component.ts
@@ -4,6 +4,8 @@ import {NgForOf} from "@angular/common";
 import {BODCatalogService} from "../../service/bodcatalog.service";
 import {getDownloadURL, ref, Storage, uploadBytes} from "@angular/fire/storage";
 import {error} from "@angular/compiler-cli/src/transformers/util";
+import Swal from "sweetalert2";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-formulario-add',
@@ -20,8 +22,10 @@ export class FormularioAddComponent {
 
   categories = ['Manga', 'Comic', 'Anime', 'Figura'];
   productForm:FormGroup;
+  foto:String;
   constructor(private catalogService:BODCatalogService,
-              private storage:Storage) {
+              private storage:Storage,
+              private router:Router) {
     file:File;
     this.productForm = new FormGroup({
       nombre:new FormControl(''),
@@ -30,6 +34,7 @@ export class FormularioAddComponent {
       descripcion: new FormControl(''),
       categoria: new FormControl(this.categories[0])
     });
+    this.foto="";
   }
   async addIMG(){
     const imgRef=ref(this.storage,`${this.file?.name}`);
@@ -44,8 +49,19 @@ export class FormularioAddComponent {
 
   }
   file:File|null =null;
-  uploadImage($event: any){
-     this.file= $event.target.files[0];
+   async uploadImage($event: any){
+     if($event.target.files && $event.target.files[0]){
+       this.file= $event.target.files[0];
+       const reader= new FileReader();
+       reader.onload=((event: ProgressEvent<FileReader>)=>{
+         if(event.target){
+           this.foto=event.target.result as String;
+         }
+       });
+       reader.readAsDataURL($event.target.files[0]);
+
+
+     }
 
 
   }
@@ -55,11 +71,23 @@ export class FormularioAddComponent {
     console.log(this.productForm);
     await this.delay(5000);
     await this.catalogService.addProduct(this.productForm.value);
+    Swal.fire({
+      icon: 'success',
+      text: 'Producto añadido con éxito.',
+      willClose: () => {
+        // Aquí va tu función navigate
+        this.router.navigate(['admin']);
+      }
+    });
 
 
   }
   delay(ms: number) {
     return new Promise(resolve => setTimeout(resolve, ms));
+  }
+  cancelClick(){
+    this.router.navigate(['admin']);
+
   }
 
 }
