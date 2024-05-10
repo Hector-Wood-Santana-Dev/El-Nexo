71eb73ff56644eb8c6a883b031be393eaba5e062
itzha <itzhakprieto@gmail.com>
2024-05-10 14:01:06 +0100

    Gastos de envio

--- src/app/Paginas/trolley/trolley.component.ts
+++ src/app/Paginas/trolley/trolley.component.ts
@@ -18,14 +18,29 @@ import {Router} from "@angular/router";
 export class TrolleyComponent implements OnInit{
   trolley:Product[]
   total:number;
+  impuestos:number;
+  gastosEn:number;
+  pagar:number;
+
   constructor(protected trolleyService:TrolleyServiceService, private router:Router) {
     this.trolley=[]
     this.total=0
+    this.impuestos=0;
+    this.gastosEn=0;
+    this.pagar=0;
   }
   ngOnInit() {
     this.trolleyService.getTrolley().subscribe(trolleys=>
       this.trolley=trolleys);
     this.trolleyService.getTotal().subscribe(total=> this.total=total);
+    if (this.total >100){
+      this.gastosEn=0;
+    }else {
+      this.gastosEn=10;
+    }
+    this.impuestos= parseFloat((this.total*0.07).toFixed(2));
+    this.pagar=this.impuestos+this.impuestos+this.total;
+
   }
 
   clickDelete(product:Product){
